8210223c1772992f37f047301f902a96ef21215e
Guillitte Matthias <matthias@guillitte.com>
2023-07-07 18:44:27 +0200

    feat: :construction: Began footer

---
+++ src/lib/components/layout/footer/Footer.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import FooterIcon from './FooterIcon.svelte';
+
+	const icons = {
+		'github-mark.svelte': { href: 'https://github.com/Ninluc', alt: 'Github' }
+	};
+
+	const iconsFolder: string = './footerIcons/';
+</script>
+
+	<section class="icons-container">
+		{#each Object.entries(icons) as [icon, attr]}
+			<FooterIcon imgPath={iconsFolder + icon} alt={attr.alt} href={attr.href} />
+		{/each}
+	</section>
+	<section class="links-container" />
+</footer>
+
+<style lang="scss">
+	footer {
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		background-color: $color-completelyblack;
+
+		// REMOVEME
+		color: green;
+
+		section.icons-container {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			column-gap: $gap-medium;
+		}
+	}
+</style>
---
+++ src/lib/components/layout/footer/FooterIcon.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let imgPath: string = '';
+	export let alt: string = '';
+	export let href: string = '/';
+
+	const sleep = (ms: number) => new Promise((f) => setTimeout(f, ms));
+
+	let Thing: any;
+
+	onMount(async () => {
+		await sleep(1000); // simulate network delay
+		Thing = (await import(/* @vite-ignore */ imgPath)).default;
+	});
+</script>
+
+<a {href} target="_blank" title={alt}>
+	<!-- <img src={imgPath} {alt} /> -->
+	<svelte:component this={Thing} />
+</a>
+
+<style lang="scss">
+	a {
+		aspect-ratio: 1 / 1;
+		width: min(42px, 10vw);
+		height: min(42px, 10vw);
+
+		// position: relative;
+
+		:global(svg) {
+			// position: absolute;
+			// top: 0px;
+			// left: 0px;
+			width: 100%;
+			height: 100%;
+
+			fill: $color-primary-500;
+
+			transition: fill $transition-time-small $transition-timingfunction;
+		}
+
+		&:hover {
+			:global(svg) {
+				fill: $color-primary-200;
+			}
+		}
+	}
+</style>
