6f37a42550d736901dbd9608d54199e02f57f67c
Guillitte Matthias <matthias@guillitte.com>
2023-07-10 16:25:48 +0200

    fix: :twisted_rightwards_arrows: Fixed CI integration test
    Just forgor to push the +page.svelte, thus creating an error on build

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,14 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	retries: process.env.CI ? 2 : 0, // set to 2 when running on CI,
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm run build && pnpm run preview',
 		timeout: 120 * 1000, // time to launch server, 6o by default
-		port: 4173
+		port: 4173,
+		reuseExistingServer: !process.env.CI
+		// stdout: 'pipe'
+	},
+	use: {
+		baseURL: 'http://localhost:4173'
 	},
 	timeout: 30000,
 	testDir: 'tests',
