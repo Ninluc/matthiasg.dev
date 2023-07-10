d098ba1872efbfaf7bc4f9a9cade7bed5153a76e
Guillitte Matthias <matthias@guillitte.com>
2023-07-10 15:26:48 +0200

    fix: :construction_worker: Should give more trace on why the CI fails

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	retries: process.env.CI ? 2 : 0, // set to 2 when running on CI,
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		prerender: {
-			handleMissingId: 'warn'
+			handleMissingId: 'warn',
+			handleHttpError: 'warn' // Will fix the CI for the moment
 		},
 		alias: {
 			$components: 'src/lib/components',
