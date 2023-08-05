c151f80cc91878f1243df2b00f08cee4e1efe69c
Guillitte Matthias <matthias@guillitte.com>
2023-08-05 17:15:06 +0200

    feat: :sparkles: Added hugo funding

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -19,6 +19,22 @@
 <WorksList works={data.works} />
 <BigTextSection>Vous voulez me contacter ?</BigTextSection>
 <ContactForm formData={data.form} />
+<ContentAfterBigTextSection
+	><iframe
+		width="100%"
+		height="480px"
+		src="https://www.watchisup.fr/compte-a-rebours/embed/quitter-la-maison-2023-09-01-00-00?backgroundcolor=%23232328&color=%23d4d6d7"
+		frameborder="0"
+		allowfullscreen
+		title="temps restant au petit hugo"
+	/>
+	<Button
+		href="https://paypal.me/hugostevens96"
+		target="_blank"
+		rel="noopener noreferrer"
+		title="T'as pas liké les petits pandas ?">Faire un don</Button
+	>
+</ContentAfterBigTextSection>
 
 <style lang="scss">
 </style>
