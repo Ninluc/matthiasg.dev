faa10eb951020d5671bb767665715ebd741a9d50
Guillitte Matthias <matthias@guillitte.com>
2023-08-06 17:55:25 +0200

    refactor: :truck: Moved Hugo funding to a page apart

---
+++ src/routes/(app)/panda/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { PREVIEW_IMG_FOLDER } from '$lib/scripts/layout/getWorkPreviewImg';
+	import ContentAfterBigTextSection from '$components/layout/main/contentSection/ContentAfterBigTextSection.svelte';
+	import ContentSection from '$components/layout/main/contentSection/base/ContentSection.svelte';
+	import Button from '$components/theme/Button.svelte';
+</script>
+
+<ContentAfterBigTextSection
+	><iframe
+		width="100%"
+		height="480px"
+		src="https://www.watchisup.fr/compte-a-rebours/embed/quitter-la-maison-2023-09-01-00-00?backgroundcolor=%23232328&color=%23d4d6d7"
+		frameborder="0"
+		allowfullscreen
+		title="temps restant au petit hugo"
+	/>
+</ContentAfterBigTextSection>
+<ContentSection id="donation">
+	<p>...c'est le temps restant avant que Hugo ne se fasse expulser de son foyer.</p>
+	<p>Pour le soutenir, faites un don :</p>
+	<Button
+		href="https://paypal.me/hugostevens96"
+		target="_blank"
+		rel="noopener noreferrer"
+		title="T'as pas liké les petits pandas ?">Faire un don</Button
+	>
+</ContentSection>
+
+<style lang="scss">
+	:global(main > div > section) {
+		height: auto;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		font: $main;
+		color: $color-primary;
+
+		p {
+			width: 100%;
+			text-align: left;
+		}
+
+		iframe {
+			display: block;
+			min-height: 480px;
+		}
+	}
+</style>
