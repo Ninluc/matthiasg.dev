c284319e4faaab3c1d0f5b69ddfbbaf5ea23f242
Guillitte Matthias <matthias@guillitte.com>
2023-08-07 14:19:42 +0200

    feat: :sparkles: Added getting the boot file on load

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -1,10 +1,16 @@
 // export const prerender = true;
 
-export const load = ({ url }) => {
+export const load = async (event) => {
 	// Get the url pathname
-	const { pathname } = url;
+	const { pathname } = event.url;
+
+	// Get the loading text
+	const loadingTextResponse = await event.fetch('/assets/boot.txt');
+	const loadingText = await loadingTextResponse.text();
+	const loadingTextLines = loadingText.split(/\r?\n/);
 
 	return {
-		pathname: pathname
+		pathname: pathname,
+		loadingText: loadingTextLines
 	};
 };
