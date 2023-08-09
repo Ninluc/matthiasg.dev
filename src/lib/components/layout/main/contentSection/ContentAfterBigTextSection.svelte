2ad72f9686a431c89921a9098585f56e28ee50dd
Guillitte Matthias <matthias@guillitte.com>
2023-08-09 12:47:20 +0200

    refactor: :recycle: Moved css declaration from component to stylesheet

--- src/lib/components/layout/main/contentSection/ContentAfterBigTextSection.svelte
+++ src/lib/components/layout/main/contentSection/ContentAfterBigTextSection.svelte
@@ -14,13 +14,3 @@
 >
 	<slot />
 </ContentSectionClassAppearOnce>
-
-<style lang="scss">
-	:global(main section.content-section) {
-		$padding-top: $gap-huge;
-		padding: $padding-top 0 ($padding-top * 2) 0;
-
-		// REMOVEME
-		font-size: x-large;
-	}
-</style>
