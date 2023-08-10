73e2366c0acd6d61b70c762821700018a789d34f
Guillitte Matthias <matthias@guillitte.com>
2023-08-10 19:08:54 +0200

    feat: :lipstick: Added styling to li; ul and code

--- src/routes/(app)/projects/[slug]/+page.svelte
+++ src/routes/(app)/projects/[slug]/+page.svelte
@@ -71,6 +71,7 @@
 			align-items: flex-start;
 			gap: $gap-medium;
 
+			// background: $color-completelyblack;
 			background: $color-completelyblack;
 			// background: $color-surface;
 
@@ -113,7 +114,8 @@
 		}
 
 		:global(p),
-		:global(p > a) {
+		:global(p > a),
+		:global(ul > li) {
 			@media only screen and (max-width: $screen-small) {
 				font-size: 18px;
 			}
@@ -132,5 +134,83 @@
 				color: darken($color-primary-500, $y * 9);
 			}
 		}
+
+		// Images
+		:global(img) {
+			border-radius: $theme-borderradius;
+			margin: $gap-small 0 $gap-medium 0;
+
+			max-width: 100%;
+
+		}
+
+		// Code
+		:global(pre) {
+			width: 100%;
+
+			padding: $gap-medium;
+
+			font-size: 1.1rem;
+
+			background: $color-completelyblack;
+			// background: $color-surface;
+
+			border-radius: $theme-borderradius;
+
+			overflow-x: auto;
+
+			@media only screen and (max-width: $screen-small) {
+				font-size: 14.4px;
+			}
+		}
+	}
+
+	// Code color
+	:global(:root) {
+		--shiki-color-text: #{$color-primary-500};
+		--shiki-color-background: #{$color-secondary};
+		--shiki-token-constant: #{$color-tertiary};
+		--shiki-token-string: #{$color-warning-300};
+		--shiki-token-comment: #{$color-surface-400};
+		--shiki-token-keyword: #{$color-success-500};
+		--shiki-token-parameter: #1eff00;
+		--shiki-token-function: #{$color-warning-600};
+		--shiki-token-string-expression: #{$color-primary-700};
+		--shiki-token-punctuation: #{$color-primary-600};
+		--shiki-token-link: #{$color-primary-800};
+
+		/* Only required if using ansiToHtml function */
+		--shiki-color-ansi-black: #000000;
+		--shiki-color-ansi-black-dim: #00000080;
+		--shiki-color-ansi-red: #bb0000;
+		--shiki-color-ansi-red-dim: #bb000080;
+		--shiki-color-ansi-green: #00bb00;
+		--shiki-color-ansi-green-dim: #00bb0080;
+		--shiki-color-ansi-yellow: #bbbb00;
+		--shiki-color-ansi-yellow-dim: #bbbb0080;
+		--shiki-color-ansi-blue: #0000bb;
+		--shiki-color-ansi-blue-dim: #0000bb80;
+		--shiki-color-ansi-magenta: #ff00ff;
+		--shiki-color-ansi-magenta-dim: #ff00ff80;
+		--shiki-color-ansi-cyan: #00bbbb;
+		--shiki-color-ansi-cyan-dim: #00bbbb80;
+		--shiki-color-ansi-white: #eeeeee;
+		--shiki-color-ansi-white-dim: #eeeeee80;
+		--shiki-color-ansi-bright-black: #555555;
+		--shiki-color-ansi-bright-black-dim: #55555580;
+		--shiki-color-ansi-bright-red: #ff5555;
+		--shiki-color-ansi-bright-red-dim: #ff555580;
+		--shiki-color-ansi-bright-green: #00ff00;
+		--shiki-color-ansi-bright-green-dim: #00ff0080;
+		--shiki-color-ansi-bright-yellow: #ffff55;
+		--shiki-color-ansi-bright-yellow-dim: #ffff5580;
+		--shiki-color-ansi-bright-blue: #5555ff;
+		--shiki-color-ansi-bright-blue-dim: #5555ff80;
+		--shiki-color-ansi-bright-magenta: #ff55ff;
+		--shiki-color-ansi-bright-magenta-dim: #ff55ff80;
+		--shiki-color-ansi-bright-cyan: #55ffff;
+		--shiki-color-ansi-bright-cyan-dim: #55ffff80;
+		--shiki-color-ansi-bright-white: #ffffff;
+		--shiki-color-ansi-bright-white-dim: #ffffff80;
 	}
 </style>
