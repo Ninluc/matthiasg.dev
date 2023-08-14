01bcf6d0d88552d1304a246fcf3f94aab21a573c
Guillitte Matthias <matthias@guillitte.com>
2023-08-14 18:05:57 +0200

    refactor: :coffin: Removed unused imports

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,12 +1,9 @@
 <script>
 	import BigTextSection from '$components/layout/main/contentSection/BigTextSection.svelte';
-	import ContentAfterBigTextSection from '$components/layout/main/contentSection/ContentAfterBigTextSection.svelte';
-	import ContentSection from '$components/layout/main/contentSection/base/ContentSection.svelte';
 	import HelloHeadsSection from '$components/layout/main/contentSection/helloHeads/HelloHeadsSection.svelte';
 	import ContactForm from '$components/layout/main/contentSection/others/ContactForm.svelte';
 	import Profiler from '$components/layout/main/contentSection/others/Profiler.svelte';
 	import WorksList from '$components/layout/main/contentSection/others/WorksList.svelte';
-	import Button from '$components/theme/Button.svelte';
 
 	export let data;
 </script>
