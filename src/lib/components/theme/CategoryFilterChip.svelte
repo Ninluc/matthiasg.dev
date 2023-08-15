3411a8ca4a8fbe9c76518b042bf734a7969662e3
Guillitte Matthias <matthias@guillitte.com>
2023-08-15 16:16:01 +0200

    feat: :sparkles: Added work CategoryFilterChip

---
+++ src/lib/components/theme/CategoryFilterChip.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import type { ProjectType } from '$lib/types/worksApi';
+	import { categoryColors } from '$stores/layout/worksCategory';
+
+	export let category: ProjectType;
+
+	let currentCategory: string | null;
+	$: currentCategory = $page.url.searchParams.get('category');
+	let isCurrentCategory: boolean;
+	$: isCurrentCategory =
+		(currentCategory == null && category == 'tous') || currentCategory == category;
+</script>
+
+<a
+	href="./projects?category={category}"
+	class="category"
+	class:current={isCurrentCategory}
+	style="--background-color: {categoryColors[category].background}; --hover-color: {categoryColors[
+		category
+	].color};"
+>
+	#{category}
+</a>
+
+<style lang="scss">
+	.category {
+		padding: $gap-medium;
+
+		font: $main;
+		line-height: $lineheight-main;
+
+		color: $color-primary;
+
+		text-decoration: none;
+
+		border: solid 3px var(--background-color);
+		border-radius: $theme-borderradius;
+
+		cursor: pointer;
+
+		transition: all $transition-time-small $transition-timingfunction;
+
+		&:hover {
+			color: #fff;
+		}
+
+		&:active,
+		&.current {
+			background-color: var(--background-color);
+			color: var(--hover-color);
+		}
+	}
+</style>
