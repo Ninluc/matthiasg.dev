1424dee6b414fca87815dba9802c4783ea78f8f3
Guillitte Matthias <matthias@guillitte.com>
2023-08-16 11:17:59 +0200

    feat: :sparkles: Added visually hidden component

---
+++ src/lib/components/theme/misc/visuallyHiddenTitle.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export const headingLevel: number = 3;
+	const heading = 'h' + headingLevel.toString();
+</script>
+
+<svelte:element this={heading} class=".visually-hidden"><slot /></svelte:element>
