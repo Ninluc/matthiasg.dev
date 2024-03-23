44ce535c6c7c0cee1e56de5c78b77fd6f3dc1edc
Matthias Guillitte <matthias@guillitte.com>
2024-03-23 11:35:41 +0100

    refactor: :lipstick: Made the whole article card a link to the article

--- src/lib/components/layout/main/contentSection/others/WorkArticle.svelte
+++ src/lib/components/layout/main/contentSection/others/WorkArticle.svelte
@@ -25,156 +25,164 @@
 </script>
 
 <article class="work">
-	<div class="image-container">
-		<Img
-			src={previewImg}
-			alt={previewAlt}
-			loading="eager"
-			bind:this={previewImgTag}
-			on:load={() => {
-				previewImgTag['$$'].ctx[0].style.background = '';
-			}}
-		/>
-		{#if previewImgHover}
+	<a href="/projects/{work.slug}">
+
+		<div class="image-container">
 			<Img
-				src={previewImgHover}
-				alt={previewAltHover}
+				src={previewImg}
+				alt={previewAlt}
 				loading="eager"
-				bind:this={previewImgHoverTag}
+				bind:this={previewImgTag}
 				on:load={() => {
-					// console.log((previewImgHoverTag['$$'].ctx[0].style.background = ''));
-					previewImgHoverTag['$$'].ctx[0].style.background = '';
+					previewImgTag['$$'].ctx[0].style.background = '';
 				}}
 			/>
-		{/if}
-		<!-- {/if} -->
-		<!-- {/await} -->
-	</div>
-	<div class="text-container">
-		<svelte:element this={heading} class="work-title" data-nosnippet>{work.title}</svelte:element>
-
-		<!-- <p class="description">{@html work.description}</p>-->
-		<!-- CHANGEME when update fix the @html issue https://github.com/kelvinsjk/sveltekitHTMLTag -->
-		<!-- Bug description : The description doesn't 'swap' with the rest of the content -->
-		<p class="description" data-nosnippet>{work.description}</p>
-
-		<div class="button-container">
-			{#if work.projectLink}
-				<Button
-					href={work.projectLink}
-					colorType="secondary"
-					target="blank"
-					rel="noopener noreferrer external"
-					>{@html work.projectLink.startsWith('https://github.com/')
-						? 'Accéder au dépôt'
-						: 'Accéder au site'}</Button
-				>
+			{#if previewImgHover}
+				<Img
+					src={previewImgHover}
+					alt={previewAltHover}
+					loading="eager"
+					bind:this={previewImgHoverTag}
+					on:load={() => {
+						// console.log((previewImgHoverTag['$$'].ctx[0].style.background = ''));
+						previewImgHoverTag['$$'].ctx[0].style.background = '';
+					}}
+				/>
 			{/if}
-			<Button href="/projects/{work.slug}" type="cta">Lire plus</Button>
+			<!-- {/if} -->
+			<!-- {/await} -->
 		</div>
-	</div>
+		<div class="text-container">
+			<svelte:element this={heading} class="work-title" data-nosnippet>{work.title}</svelte:element>
+	
+			<!-- <p class="description">{@html work.description}</p>-->
+			<!-- CHANGEME when update fix the @html issue https://github.com/kelvinsjk/sveltekitHTMLTag -->
+			<!-- Bug description : The description doesn't 'swap' with the rest of the content -->
+			<p class="description" data-nosnippet>{work.description}</p>
+	
+			<div class="button-container">
+				{#if work.projectLink}
+					<Button
+						href={work.projectLink}
+						colorType="secondary"
+						target="blank"
+						rel="noopener noreferrer external"
+						>{@html work.projectLink.startsWith('https://github.com/')
+							? 'Accéder au dépôt'
+							: 'Accéder au site'}</Button
+					>
+				{/if}
+				<Button href="/projects/{work.slug}" type="cta">Lire plus</Button>
+			</div>
+		</div>
+	</a>
 </article>
 
 <style lang="scss">
 	article.work {
-		display: flex;
-		flex-direction: row;
-		align-items: stretch;
-
+		
 		background-color: $color-secondary;
 		border-radius: $theme-borderradius;
-
+		
 		overflow: hidden;
+		
+		a {
+			display: flex;
+			flex-direction: row;
+			align-items: stretch;
+			text-decoration: none;
 
-		div.image-container {
-			width: 31%;
-
-			aspect-ratio: 1 / 1;
-
-			position: relative;
-
-			:global(picture) {
-				width: 100%;
-				height: 100%;
-
-				:global(img) {
+			div.image-container {
+				width: 31%;
+	
+				aspect-ratio: 1 / 1;
+	
+				position: relative;
+	
+				:global(picture) {
 					width: 100%;
 					height: 100%;
-					object-fit: contain;
-
-					background-color: $color-secondary !important;
-
-					--reveal-scale: 1.03;
-					--reveal-opacity-duration: 1s;
-					--reveal-transform-duration: 0.6s;
+	
+					:global(img) {
+						width: 100%;
+						height: 100%;
+						object-fit: contain;
+	
+						background-color: $color-secondary !important;
+	
+						--reveal-scale: 1.03;
+						--reveal-opacity-duration: 1s;
+						--reveal-transform-duration: 0.6s;
+					}
 				}
-			}
-
-			:global(picture:nth-child(2)) {
-				position: absolute;
-				top: 0;
-				left: 0;
-
-				:global(img) {
-					opacity: 0;
-
-					object-fit: cover;
-
-					pointer-events: none;
-					z-index: 5;
-
-					transition: opacity $transition-time-medium $transition-timingfunction;
-				}
-			}
-		}
-
-		div.text-container {
-			padding: $theme-borderradius;
-			flex: 1;
-
-			display: flex;
-			flex-direction: column;
-			justify-content: flex-start;
-			gap: $gap-medium;
-
-			.work-title {
-				margin: 0;
-
-				font: $headerlink;
-				color: $color-primary;
-				// font-size: 32px;
-
-				transform: skewX(-9deg);
-			}
-
-			p.description {
-				display: block;
-
-				font: $main;
-				color: $color-primary;
-
-				:global(a) {
-					@include linkMain;
-					font-style: normal;
-					color: $color-tertiary;
-
-					transition: color $transition-time-small $transition-timingfunction;
-
-					&:hover,
-					&:focus {
-						color: $color-tertiary-700;
+	
+				:global(picture:nth-child(2)) {
+					position: absolute;
+					top: 0;
+					left: 0;
+	
+					:global(img) {
+						opacity: 0;
+	
+						object-fit: cover;
+	
+						pointer-events: none;
+						z-index: 5;
+	
+						transition: opacity $transition-time-medium $transition-timingfunction;
 					}
 				}
 			}
-
-			div.button-container {
-				margin-top: auto;
-
+	
+			div.text-container {
+				padding: $theme-borderradius;
+				flex: 1;
+	
 				display: flex;
-				flex-direction: row;
-				justify-content: flex-end;
+				flex-direction: column;
+				justify-content: flex-start;
 				gap: $gap-medium;
+	
+				.work-title {
+					margin: 0;
+	
+					font: $headerlink;
+					color: $color-primary;
+					// font-size: 32px;
+	
+					transform: skewX(-9deg);
+				}
+	
+				p.description {
+					display: block;
+	
+					font: $main;
+					color: $color-primary;
+	
+					:global(a) {
+						@include linkMain;
+						font-style: normal;
+						color: $color-tertiary;
+	
+						transition: color $transition-time-small $transition-timingfunction;
+	
+						&:hover,
+						&:focus {
+							color: $color-tertiary-700;
+						}
+					}
+				}
+	
+				div.button-container {
+					margin-top: auto;
+	
+					display: flex;
+					flex-direction: row;
+					justify-content: flex-end;
+					gap: $gap-medium;
+				}
 			}
+	
 		}
 
 		&:hover,
@@ -196,31 +204,34 @@
 
 			overflow: hidden;
 
-			div.image-container {
-				position: absolute;
+			a {
 
-				top: 0px;
-				left: 0px;
-
-				width: 100%;
-
-				transform: translateY(-40%);
-
-				&::after {
-					content: '';
+				div.image-container {
 					position: absolute;
-					top: 0;
-					left: 0;
-
+	
+					top: 0px;
+					left: 0px;
+	
 					width: 100%;
-					height: 100%;
-
-					background-image: linear-gradient(0deg, rgba(30, 31, 35, 1) 23%, rgba(30, 31, 35, 0) 80%);
+	
+					transform: translateY(-40%);
+	
+					&::after {
+						content: '';
+						position: absolute;
+						top: 0;
+						left: 0;
+	
+						width: 100%;
+						height: 100%;
+	
+						background-image: linear-gradient(0deg, rgba(30, 31, 35, 1) 23%, rgba(30, 31, 35, 0) 80%);
+					}
 				}
-			}
-
-			div.text-container {
-				z-index: 100;
+	
+				div.text-container {
+					z-index: 100;
+				}
 			}
 		}
 	}
