91d48bab82c3ba4b50eda9853b5b1c58bbf5cd53
Matthias Guillitte <matthias@guillitte.com>
2026-09-22 18:43:18 +0200

    Removed empty work category filter

--- src/routes/(app)/projects/+page.svelte
+++ src/routes/(app)/projects/+page.svelte
@@ -21,7 +21,7 @@
 			<CategoryFilterChip category="tous" />
 			<CategoryFilterChip category="personnel" />
 			<CategoryFilterChip category="scolaire" />
-			<CategoryFilterChip category="professionnel" />
+			<!-- <CategoryFilterChip category="professionnel" /> -->
 		</nav>
 	</div>
 </TitleHeader>
